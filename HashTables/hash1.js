bfcd8c172cc8bb618eac71e2138a6aec136f7f04
Jahongir Hamidov <hamidovjahongir444@gmail.com>
2023-12-23 14:57:01 +0500

    new

--- HashTables/hash1.js
+++ HashTables/hash1.js
@@ -19,7 +19,7 @@ class HashTable{
     constructor(size){
         this.data = new Array(size)
     }
-    _hash(key){
+    _hash(key){  // _ is a common standard that you can access this method outside the class
         let hash = 0
         for (let i=0;i<key.length;i++){
             hash = (hash + key.charCodeAt(i)*i)% this.data.length
