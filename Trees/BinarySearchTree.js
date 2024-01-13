f54a5bc8fcd4abbcf03c9732d6f541f2296e8d94
Jahongir Hamidov <hamidovjahongir444@gmail.com>
2024-01-13 21:01:40 +0500

    binary tree

---
+++ Trees/BinarySearchTree.js
@@ -0,0 +1,63 @@
+class Node{
+    constructor(value) {
+        this.value = value
+        this.left = null
+        this.right = null   
+    }
+}
+
+class BinarySearchTree{
+    constructor() {
+        this.root = null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if (!this.root){
+            this.root = newNode
+        } else {
+            let currentNode = this.root
+            while(true){
+                if(value<currentNode.value){
+                    // Left
+                    if(!currentNode.left){
+                        currentNode.left = newNode
+                        return this
+                    }
+                    currentNode = currentNode.left
+                } else {
+                    // Right
+                    if(!currentNode.right){
+                        currentNode.right = newNode
+                        return this
+                    }
+                    currentNode = currentNode.right
+                }
+            }
+        }
+    }
+    lookup(value){
+
+    }
+    remove(value){
+
+    }
+}
+
+const tree = new BinarySearchTree()
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(1)
+tree.insert(20)
+tree.insert(15)
+tree.insert(170)
+
+console.log(JSON.stringify(traverse(tree.root)))
+function traverse(node){
+    const tree = {value: node.value}
+    tree.left = node.left === null ? null :
+    traverse(node.left)
+    tree.right = node.right === null ? null :
+    traverse(node.right)
+    return tree
+}
