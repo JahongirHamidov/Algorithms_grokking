6aae5da5afcaaee3e3a438494ef535ae5829e801
Jahongir Hamidov <hamidovjahongir444@gmail.com>
2024-01-18 22:38:57 +0500

    caching

---
+++ Data Structures/Algorithms/DynamicProgramming/caching.js
@@ -0,0 +1,16 @@
+function addTo80(n) {
+    return n+80
+}
+
+let cache = {}
+
+function memoizedAddTo80(n) {
+    if(n in cache){
+        return cache[n]
+    } else{
+        cache[n] = n+80
+        return cache[n]
+    }
+}
+
+console.log(memoizedAddTo80(5))
